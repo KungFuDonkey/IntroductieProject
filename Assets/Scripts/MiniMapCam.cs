6abf6ce31ae223f934bc75f52641fe4efd010057
Karst van der Wal <k.a.vanderwal@students.uu.nl>
2019-12-11 15:06:10 +0100

    evolve and minimap

--- Assets/Scripts/MiniMapCam.cs
+++ Assets/Scripts/MiniMapCam.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class MiniMapCam : MonoBehaviour
 {
-    [SerializeField] private Transform playerTransform;
+    protected Transform playerTransform;
     [SerializeField] private float yOffset;
+    void start()
+    {
+        playerTransform = gameObject.GetComponent<Transform>();
+        Debug.Log(gameObject.GetComponent<Transform>().name);
+    }
     private void LateUpdate()
     {
         Vector3 targetPosition = playerTransform.position;
