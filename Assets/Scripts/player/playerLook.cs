1889e598c93585713220c511e9c41be34777720f
Karst van der Wal <k.a.vanderwal@students.uu.nl>
2019-12-11 14:28:02 +0100

    merge

--- Assets/Scripts/player/playerLook.cs
+++ Assets/Scripts/player/playerLook.cs
@@ -19,8 +19,7 @@ public class playerLook : MonoBehaviour
     protected float qAbility, QABILITY;
     protected float evolveXP = 0f, evolveXPNeeded = 1000f, xpGenerator = 200f, evolveTime;
     protected bool canEvolve = true, hover = false, evolving = false;
-    protected Transform avatarTrans, localTrans;
-
+    public Transform avatarTrans, localTrans;
 
     // Start is called before the first frame update
     void Start()
