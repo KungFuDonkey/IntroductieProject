3b30d61d162f94f2936b439ea5b6736061d81ee1
Karst van der Wal <k.a.vanderwal@students.uu.nl>
2020-01-15 11:45:54 +0100

    walls

--- Assets/Scripts/server/Walls.cs
+++ Assets/Scripts/server/Walls.cs
@@ -5,32 +5,75 @@ using UnityEngine;
 public class Walls
 {
     public static Transform[] walls = new Transform[4];
-    protected static float wallsWait = 10;
-    protected static float wallsMove = 10;
+    protected static float wallsWait, WALLSWAIT = 10, wallsMove, WALLSMOVE = 10, mapSize = 600;
     public static Vector3 circlePosition;
     public static Vector3[] distances = new Vector3[4];
-    static bool started = false;
-    public static void Start()
-    {
-        
-    }
+    static bool started = false, allWallsStop = true, waiting = false;
+    static bool[] wallMoving = new bool[4];
+
     public static void UpdateWalls()
     {
         if (!started)
         {
             started = true;
-            circlePosition = new Vector3(Random.Range(-250, 250), 20, Random.Range(-250, 250));
-            Debug.Log(circlePosition.ToString());
-            for (int i = 0; i < 4; i++)
-            {
-                distances[i] = circlePosition - walls[i].position;
-            }
+            mapSize *= 0.5f;
+            setDistances(mapSize, mapSize);
         }
-        for (int i = 0; i < 4; i++)
+        else if (!allWallsStop)
         {
-            walls[i].position += distances[i] * Time.deltaTime / 60;
+            wallsMove -= Time.deltaTime;
+        }
+        else
+        {
+            wallsWait -= Time.deltaTime;
         }
 
+        if (allWallsStop && wallsWait < 0 && !waiting)
+        {
+            mapSize *= 0.5f;
+            setDistances(circlePosition.x + mapSize, circlePosition.z + mapSize);
+            wallsWait = WALLSWAIT;
+            waiting = true;
+        }
+        else if (wallsWait < 0 && allWallsStop)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                wallMoving[i] = true;
+            }
+            wallsMove = WALLSMOVE;
+            waiting = false;
+        }
+
+        if (!allWallsStop)
+        {
+            moveWalls();
+        }
         ServerSend.SetWalls();
+    }
+
+    public static void moveWalls()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Vector3.Distance(circlePosition, walls[i].position) > mapSize / 2)
+            {
+                walls[i].position += distances[i] * Time.deltaTime / 10;
+            }
+            else
+            {
+                wallMoving[i] = false;
+            }
+        }
+    }
+    public static void setDistances(float x, float z)
+    {
+        allWallsStop = true;
+        circlePosition = new Vector3(Random.Range(-x/2, x/2), 20, Random.Range(-z/2, z/2));
+        for (int i = 0; i < 4; i++)
+        {
+            distances[i] = circlePosition - walls[i].position;
+        }
+        Debug.Log(circlePosition.ToString());
     }
 }
